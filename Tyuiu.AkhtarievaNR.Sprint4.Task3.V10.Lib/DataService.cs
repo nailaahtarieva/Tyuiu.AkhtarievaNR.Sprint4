7db73785ae1e6fbf5d5acb75018633d6ccd986be
nailaahtarieva <nuscha2006@mail.ru>
2024-11-14 11:50:55 +0500

    Task3

---
+++ Tyuiu.AkhtarievaNR.Sprint4.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AkhtarievaNR.Sprint4.Task3.V10.Lib
+{
+    public class DataService : ISprint4Task3V10
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int cols = array.Length / rows;
+
+            int maxi = int.MinValue;
+
+            for (int i = 2; i < 3; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (array[i,j] > maxi)
+                    {
+                        maxi = array[i,j];
+                    }
+                }
+            }
+            return maxi;
+        }
+    }
+}
---
+++ Tyuiu.AkhtarievaNR.Sprint4.Task3.V10/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.AkhtarievaNR.Sprint4.Task3.V10.Lib;
+
+namespace Tyuiu.AkhtarievaNR.Sprint4.Task3.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5] { { 5, 7, 6, 6, 5 }, { 5, 5, 5, 4, 6 }, { 8, 5, 7, 4, 7 }, { 7, 6, 7, 4, 7 }, { 5, 8, 7, 8, 8 } };
+            int rows = array.GetUpperBound(0) + 1;
+            int cols = array.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Ахтариева Н. Р. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание: 3                                                              *");
+            Console.WriteLine("* Вариант: 10                                                             *");
+            Console.WriteLine("* Выполнила: Ахтариева Наиля Руслановна | ИИПб-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет максимальное число в третьей      *");
+            Console.WriteLine("* строке двумерного массива.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(array);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
