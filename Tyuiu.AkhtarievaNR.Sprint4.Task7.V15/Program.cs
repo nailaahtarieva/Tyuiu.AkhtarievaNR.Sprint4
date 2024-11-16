51aff62673c8ccc13bb856b9a90ec6afe5ae28d0
nailaahtarieva <nuscha2006@mail.ru>
2024-11-16 13:15:22 +0500

    Task7

---
+++ Tyuiu.AkhtarievaNR.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.AkhtarievaNR.Sprint4.Task7.V15.Lib;
+
+namespace Tyuiu.AkhtarievaNR.Sprint4.Task7.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int m = 2;
+            int[,] mtrx = new int[n, m];
+
+            string str = "10293847";
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Ахтариева Н. Р. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание: 7                                                              *");
+            Console.WriteLine("* Вариант: 15                                                             *");
+            Console.WriteLine("* Выполнила: Ахтариева Наиля Руслановна | ИИПб-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая преобразует строку в матрицу и подсчитывает *");
+            Console.WriteLine("* количества нечёных чисел в ней.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
